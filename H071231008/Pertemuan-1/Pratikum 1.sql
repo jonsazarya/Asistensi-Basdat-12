5d65c07ac650de585a166d7d4c21a437006ced45
AndiRiswanda <andiriswandalah.gmail@com>
2024-09-13 13:22:45 +0800

    menambahkan tugas praktikum-1

---
+++ H071231008/Pertemuan-1/Pratikum 1.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE library;
+
+CREATE TABLE authors (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE books (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	isbn CHAR(13) NOT NULL,
+	title VARCHAR(100) NOT NULL,
+	author_id INT,
+	FOREIGN KEY(author_id) REFERENCES authors(id)
+);
+
+ALTER TABLE authors
+ADD COLUMN nationality VARCHAR(50);
+
+ALTER TABLE  books
+MODIFY isbn CHAR(13) UNIQUE NOT NULL;
+
+DESCRIBE authors;
+DESCRIBE books;
+
+CREATE TABLE members (
+	id	INT PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(50) NOT NULL, 
+	last_name VARCHAR(50) NOT NULL,
+	email	VARCHAR(100) NOT NULL,
+	phone_number CHAR(10),
+	join_date DATE NOT NULL,
+	membership_type VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE books
+MODIFY title VARCHAR(150) NOT NULL,
+ADD COLUMN published_year YEAR NOT NULL,
+ADD COLUMN genre VARCHAR(50) NOT NULL,
+ADD COLUMN copies_availabe INT NOT NULL;
+
+CREATE TABLE borrowings (
+	id	INT PRIMARY KEY AUTO_INCREMENT,
+	member_id INT NOT NULL,
+	book_id INT NOT NULL,
+	borrow_date DATE NOT NULL,
+	return_date DATE NOT NULL,
+	FOREIGN KEY(member_id) REFERENCES members(id),
+	FOREIGN KEY(book_id) REFERENCES books(id)
+);
+
